d0387ae38fe4ab8bc3f0f3d8359123c819494f20
DiegoGarcia172 <i21050333@monclova.tecnm.mx>
2024-09-30 10:55:07 -0600

    d

--- mostrar.php
+++ mostrar.php
@@ -1,5 +1,9 @@
 <?php 
+    //variable por pos
     $nombre = $_POST['nombre'];
     $pass = $_POST['password'];
+    //variable por GET
+    //  $nombre = $_GET['nombre'];
+    //$pass = $_GET['password'];
     echo "Tu contrasena ingresada es: ".$pass .  " Tu nombre es: ".$nombre;
 ?>
