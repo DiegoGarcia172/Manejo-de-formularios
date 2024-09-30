9bd2094ae58d67577dbcc32ab5feb5ab08020795
DiegoGarcia172 <i21050333@monclova.tecnm.mx>
2024-09-30 11:14:26 -0600

    explicacion2

--- mostrar.php
+++ mostrar.php
@@ -7,3 +7,22 @@
     //$pass = $_GET['password'];
     echo "Tu contrasena ingresada es: ".$pass .  " Tu nombre es: ".$nombre;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel del alumno</title>
+</head>
+<body>
+        <!-- SI QUISIERAS UTILIZARLA PARA ALGUNA ETIQUETA -->
+        <h1>Hola <?php echo $nombre?></h1>
+        <!--EN EL VALOR MODIFICABLE EN UN INPUT-->
+         <input type="text" value="<?php echo $nombre?>">
+         <!-- EN UN PLACE HOLDER ALMACENADO sin modificacion -->
+          <input type="text" placeholder="<?php echo $nombre?>">
+        <!-- si quisieras en una label -->
+         <label for=""><?php echo $nombre?></label>
+
+</body>
+</html>
